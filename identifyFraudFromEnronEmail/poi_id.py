3b21e61d51c5612a20d77a1892c3f04833c7cf6e
Shanshan Wang <wangss523@gatech.edu>
2018-07-09 17:20:23 -0500

    update ml_project

--- identifyFraudFromEnronEmail/poi_id.py
+++ identifyFraudFromEnronEmail/poi_id.py
@@ -6,6 +6,9 @@ import pprint
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd 
+from scipy import stats
+import seaborn as sns
+import tester
 
 sys.path.append("../tools/")
 
@@ -59,7 +62,7 @@ data_dict.pop("THE TRAVEL AGENCY IN THE PARK",0)
 ### Task 3: Create new feature(s)
 #feature engineering
 #remove feature "email_address"
-#create new features "from_poi_to_this_person_ratio" and "from_this_person_to_poi_Ratio"
+#create new features "from_poi_to_this_person_ratio" and "from_this_person_to_poi_ratio"
 for f in data_dict:
     if data_dict[f]["from_poi_to_this_person"]!="NaN":
         data_dict[f]["from_poi_to_this_person_ratio"] = 1.0 * data_dict[f]["from_poi_to_this_person"]/data_dict[f]["to_messages"]
@@ -68,38 +71,58 @@ for f in data_dict:
         
 for f in data_dict:
     if data_dict[f]["from_this_person_to_poi"]!="NaN":
-        data_dict[f]["from_this_person_to_poi_Ratio"] = 1.0 * data_dict[f]["from_this_person_to_poi"]/data_dict[f]["from_messages"]
+        data_dict[f]["from_this_person_to_poi_ratio"] = 1.0 * data_dict[f]["from_this_person_to_poi"]/data_dict[f]["from_messages"]
     else:
-        data_dict[f]["from_this_person_to_poi_Ratio"] = "NaN"   
+        data_dict[f]["from_this_person_to_poi_ratio"] = "NaN"   
         
 #make the featurs_list
 features_list  = ['poi','total_payments','total_stock_value',
                   'salary', 'deferral_payments', 'loan_advances', 'bonus', 'restricted_stock_deferred', 'deferred_income', 
                   'expenses', 'exercised_stock_options', 'other', 'long_term_incentive', 'restricted_stock', 
                   'director_fees','to_messages', 'from_poi_to_this_person', 'from_messages', 'from_this_person_to_poi', 
-                  'shared_receipt_with_poi','from_poi_to_this_person_ratio','from_this_person_to_poi_Ratio']
+                  'shared_receipt_with_poi','from_poi_to_this_person_ratio','from_this_person_to_poi_ratio']
 
-#data_array = featureFormat(data_dict,features_list)
-#labels, features = targetFeatureSplit(data_array)
 
-#split the data into training and testing
-"""
-from sklearn import cross_validation
-from sklearn.cross_validation import StratifiedShuffleSplit
-features_train, features_test, labels_train, labels_test = cross_validation.train_test_split(features,labels, test_size = 0.3, random_state = 42)
-"""
-### Store to my_dataset for easy export below.
-
+#To further explore the dataset, transform the dictionary into a dataframe
 df = pd.DataFrame.from_dict(data_dict,orient="index")
 df = df.replace("NaN",np.nan)
 df = df[features_list]
-print df.shape
+print "This dataset has {} rows and {} columns".format(df.shape[0],df.shape[1])
+
+#impute null financial features by 0
+df.loc[:,"total_payments":"director_fees"] = df.loc[:,"total_payments":"director_fees"].fillna(0)
+
+#impute null contact features by the mean
+df.loc[:,"to_messages":"from_this_person_to_poi_ratio"] = df.loc[:,"to_messages":"from_this_person_to_poi_ratio"].apply(lambda x: x.fillna(x.mean()),axis = 0)
+
+#Check incorrect data point in "total_payments" and "total_stock_value"
+#Check the "total_payments" 
+payments = ["salary","deferral_payments","loan_advances","bonus","deferred_income",
+            "expenses","other","director_fees","long_term_incentive"]
+df[df[payments].sum(axis = "columns")!=df.total_payments] 
+#Correct total_payments
+df["total_payments"]["BELFER ROBERT"] = df[payments].loc["BELFER ROBERT"].sum()
+df["total_payments"]["BHATNAGAR SANJAY"] = df[payments].loc["BHATNAGAR SANJAY"].sum()
+
+#Check the "total_stock_value"
+stock_value = ["restricted_stock_deferred","exercised_stock_options","restricted_stock"]
+df[df[stock_value].sum(axis = "columns") !=df.total_stock_value]
+
+#Correct total_stock_value
+df["total_stock_value"]["BELFER ROBERT"] = df[stock_value].loc["BELFER ROBERT"].sum()
+df["total_stock_value"]["BHATNAGAR SANJAY"] = df[stock_value].loc["BHATNAGAR SANJAY"].sum()
+
+#final check the dataset
 df.head()
 
+### Store to my_dataset for easy export below.
+my_dataset = df.to_dict(orient = "index")
+feature_list_new = list(df.columns.values)
+print "There are {} people and {} features in this dataset".format(len(my_dataset),len(feature_list_new))
 
 ### Extract features and labels from dataset for local testing
-data = featureFormat(my_dataset, features_list, sort_keys = True)
-labels, features = targetFeatureSplit(data)
+data = featureFormat(my_dataset,feature_list_new,sort_keys=True)
+labels,features = targetFeatureSplit(data)
 
 ### Task 4: Try a varity of classifiers
 ### Please name your classifier clf for easy export below.
@@ -108,8 +131,35 @@ labels, features = targetFeatureSplit(data)
 ### http://scikit-learn.org/stable/modules/pipeline.html
 
 # Provided to give you a starting point. Try a variety of classifiers.
-from sklearn.naive_bayes import GaussianNB
-clf = GaussianNB()
+#select features using random forest
+clf = RandomForestClassifier(random_state=7)
+tester.test_classifier(clf, my_dataset,feature_list_new)
+
+#list the feature importances
+importances = clf.feature_importances_
+indices = np.argsort(importances)[::-1]
+
+features_list = sorted(zip(map(lambda x: round(x, 4), clf.feature_importances_), 
+                           feature_list_new[1:]), reverse=True)
+
+features_df = pd.DataFrame(features_list)
+features_df = features_df.rename(index = str, columns = {0:"importance",1:"feature"})
+
+#based on feature importance, select following features for modeling
+selected_features = ['poi','exercised_stock_options','from_this_person_to_poi_ratio']
+
+#Try a variatye of classifiers
+#gaussian navie bays
+nb = GaussianNB()
+tester.test_classifier(nb, my_dataset,selected_features)
+
+#decision tree
+dt = DecisionTreeClassifier()
+tester.test_classifier(dt,my_dataset,selected_features)
+
+#random forest
+rf = RandomForestClassifier()
+tester.test_classifier(rf,my_dataset,selected_features)
 
 ### Task 5: Tune your classifier to achieve better than .3 precision and recall 
 ### using our testing script. Check the tester.py script in the final project
@@ -118,10 +168,30 @@ clf = GaussianNB()
 ### stratified shuffle split cross validation. For more info: 
 ### http://scikit-learn.org/stable/modules/generated/sklearn.cross_validation.StratifiedShuffleSplit.html
 
-# Example starting point. Try investigating other evaluation techniques!
-from sklearn.cross_validation import train_test_split
-features_train, features_test, labels_train, labels_test = \
-    train_test_split(features, labels, test_size=0.3, random_state=42)
+#From task 4, decision_tree performed the best, here I will tune parameters for 
+from sklearn.cross_validation import StratifiedShuffleSplit
+from sklearn.grid_search import GridSearchCV
+
+# tune decision tree
+data = featureFormat(my_dataset, selected_features, sort_keys=True)
+labels, features = targetFeatureSplit(data)
+
+# 1000 folds are used to make it as similar as possible to tester.py.
+folds = 1000
+decision_tree = DecisionTreeClassifier()
+dt_parameters = {'min_samples_split':(5,10,15), \
+                 'max_depth':(3,5,7)}
+# store the split instance into cv and use it in the GridSearchCV.
+cv = StratifiedShuffleSplit(labels, folds)
+grid = GridSearchCV(decision_tree, dt_parameters, cv=cv, scoring='f1')
+grid.fit(features, labels)
+
+print("The best parameters are %s with a score of %0.4f"
+      %(grid.best_params_, grid.best_score_))
+
+#decision tree model with optimal parametres
+dt_best = DecisionTreeClassifier(min_samples_split=5,max_depth=7)
+tester.test_classifier(dt_best,my_dataset,selected_features)
 
 ### Task 6: Dump your classifier, dataset, and features_list so anyone can
 ### check your results. You do not need to change anything below, but make sure
@@ -129,3 +199,4 @@ features_train, features_test, labels_train, labels_test = \
 ### generates the necessary .pkl files for validating your results.
 
 dump_classifier_and_data(clf, my_dataset, features_list)
+tester.main()
